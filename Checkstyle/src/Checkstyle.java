51f02094aa33f5a770176283be9da5dbf4bff870
jesus Bedoya <jesusbeds97@gmail.com>
2020-01-10 00:34:02 +0100

    Taller

---
+++ Checkstyle/src/Checkstyle.java
@@ -0,0 +1,15 @@
+
+public class Checkstyle {
+	private int X;
+	private static int a;
+	private int Y;
+
+	public Checkstyle(int X, int Y) {
+		this.X = X;
+		this.Y = Y;
+	}
+	public int suma() {
+		return X+Y;
+	}
+
+}
